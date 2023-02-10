8a90a010c56d39d5c4782fe190ca1ded22bc0ef4
Vasiliy Gorulev <89159793029@ya.ru>
2023-02-10 21:34:33 +0300

    add seminar 9

---
+++ Seminar_9_on_C#/Task_1/Program.cs
@@ -0,0 +1,102 @@
+﻿// Задача 63: Задайте значение N. Напишите программу, 
+// которая выведет все натуральные числа в промежутке от 1 до N.
+// N = 5 -> "1, 2, 3, 4, 5"
+// N = 6 -> "1, 2, 3, 4, 5, 6"
+
+// Задача 65: Задайте значения M и N. Напишите программу, 
+// которая выведет все натуральные числа в промежутке от M до N.
+// M = 1; N = 5 -> "1, 2, 3, 4, 5"
+// M = 4; N = 8 -> "4, 6, 7, 8"
+
+
+//63
+/*
+int UserInput()
+{
+    int number;
+    while (!int.TryParse(Console.ReadLine(), out number))
+        Console.Write("Вы ввели не число. Повторите ввод: ");
+
+    return number;
+}
+System.Console.Write("Введите число: ");
+int n = UserInput();
+
+int PrintNumbers(int n)
+{
+    int[] array = new int[n];
+    // array[0] = 1;
+    array[n] = array[n - 1] + 1;
+    return array;
+}
+
+int[] result = PrintNumbers(n);
+
+void PrintArray(int[] arr)
+{
+    int count = arr.Length;
+
+    for (int i = 0; i < count - 1; i++)
+    {
+        Console.Write($"{arr[i]}, ");
+    }
+    System.Console.Write(arr[arr.Length - 1]);
+    // Console.WriteLine();
+}
+
+PrintArray(result);
+*/
+
+void PrintNumbers(int N)
+{
+    if (N <= 0)
+    {
+        return;
+    }
+    if (N == 1)
+    {
+        System.Console.WriteLine($"{N}");
+    }
+    else
+    {
+        PrintNumbers(N - 1);
+        System.Console.WriteLine($", {N}");
+    }
+}
+
+//2 задача
+/*
+Задача 65: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+M = 1; N = 5 -> "1, 2, 3, 4, 5"
+M = 4; N = 8 -> "4, 6, 7, 8"
+*/
+
+void PrintNumber(int M, int N)
+{
+    if (M > N)
+    {
+        PrintNumber(M - 1, N);
+        Console.Write($", {M}");
+    }
+    else if (M < N)
+    {
+        PrintNumber(M, N - 1);
+        Console.Write($", {N}");
+    }
+    else if (M == N)
+    {
+        Console.Write($"{M}");
+    }
+}
+
+int GetNumber()
+{
+    Console.Write($"Введите значение числа: ");
+    int.TryParse(Console.ReadLine(), out int result);
+    return result;
+}
+
+int M = GetNumber();
+int N = GetNumber();
+PrintNumber(M, N);
+
---
+++ Seminar_9_on_C#/Task_2/Program.cs
@@ -0,0 +1,69 @@
+﻿// Задача 67: Напишите программу, которая будет 
+// принимать на вход число и возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+
+// Задача 69: Напишите программу, которая на вход принимает 
+// два числа A и B, и возводит число А в целую степень B с помощью рекурсии.
+// A = 3; B = 5 -> 243 (3⁵)
+// A = 2; B = 3 -> 8
+
+int SummNum(int n)
+{
+    if (n < 10) return n;
+
+    //if (n == 0 || n == 1) return 1;
+
+    return n += SummNum(n) % 10;
+}
+int n = 7;
+System.Console.WriteLine($"Сумма {n}: ");
+
+// Задача 67. Находим сумму цифр
+
+
+Console.WriteLine("Введите число N");
+if (!int.TryParse(Console.ReadLine(), out int n) || n < 1)
+{
+    Console.WriteLine("Введено нецелое или отрицательное число или 0");
+    return;
+}
+
+
+int SumsOfNumbers(int n)
+{
+    if (n < 1) return 0;
+    return n = (n % 10) + SumsOfNumbers(n / 10);
+}
+
+Console.WriteLine(SumsOfNumbers(n));
+
+//69 тело степень числа через 
+
+int AinB(int a, int b)
+{
+    if (b == 1) return a;
+    if (b == 0) return 1;
+    return AinB(a, b - 1) * a;
+}
+
+//с отрицательной степенью
+double PowNumbers(double numA, double numB)
+{
+    if (numB == 1)
+    {
+        return numA;
+    }
+    else if (numB == 0)
+    {
+        return 1;
+    }
+    else if (numB < 0)
+    {
+        return 1 / PowNumbers(numA, Math.Abs(numB));
+    }
+    else
+    {
+        return numA * PowNumbers(numA, numB - 1);
+    }
+}
